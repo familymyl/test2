4df233a2e0b81bcbb2f7fae9cb745fa867486012
familymyl <1032791353@qq.com>
2016-12-09 08:56:31 +0800

    String 存储机制

---
+++ p2/src/p2/Test.java
@@ -0,0 +1,16 @@
+package p2;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		String x = "a"+"a";
+		String y = x + "a";
+		String z = "aa";
+		// String y = new StringBuilder(x).append("a").toString();
+		
+		System.out.println(x==y);
+		System.out.println(x==z);
+	}
+
+}
